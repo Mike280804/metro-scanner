56dc8650f8fffaaaa66fea56f40a44488bf3f7d6
Mike280804 <mieunu2882004@gmail.com>
2025-07-22 17:13:01 +0700

    fix: url socket

--- src/libs/socket.ts
+++ src/libs/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://192.168.1.9:4000", {
+const socket = io(process.env.EXPO_PUBLIC_SOCKET_URL, {
   transports: ["websocket"],
 });
 
